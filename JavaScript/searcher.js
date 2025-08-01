032e4ab59a488549d6fe4531335bf679559a71f2
3667808244 <3667808244@qq.com>
2025-08-02 00:42:42 +0800

    调整searcher.js

--- JavaScript/searcher.js
+++ JavaScript/searcher.js
@@ -1,14 +1,18 @@
 function search() {
-    console.log('search_log called');
+    console.group('Saerch called');
     const Id = document.getElementById('log_id').value.trim();
     if (Id) {
         const Element = document.getElementById(Id);
         if (Element) {
             Element.scrollIntoView({ behavior: 'smooth' });
+            console.log('Turing done')
         } else {
+            console.warn('Unknown item');
             alert('搜索项不存在');
         }
     } else {
         alert('请输入搜索项');
+        console.warn('Empty value')
     }
+    console.groupEnd();
 }
